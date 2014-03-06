927f6295745960ea6743026fe61a1ce0dace4ffb
ItalianPride15 <italianpride15@gmail.com>
2014-03-06 15:43:05 -0600

    changed abc to be abc/ndb, added interface to datastore from handlers, refactored out authentication, and removed all eclipse/pydev remains

--- models/facebook.py
+++ models/facebook.py
@@ -3,21 +3,42 @@ from baseclasses import BaseDatastore
 
 class FacebookProfile(BaseProfile, BaseDatastore):
 
-	#might want to include @properties for getters/setters
+	#let every field be indexed by default for querys
+	self.email = nbd.StringProperty()
+	self.full_name = nbd.StringProperty()
+	self.qualities = nbd.StringProperty() #list
+	self.connections = nbd.StringProperty() #list and call GQL CONTAINS #look into ReferenceProperty()
+	self.address = nbd.StringProperty()
+	self.curr_classes = nbd.StringProperty() #list, edu only
+	self.past_classes = nbd.StringProperty() #list, linkedin/old edu
+	self.major = nbd.StringProperty()
+	self.minor = nbd.StringProperty()
+	self.graduation = nbd.StringProperty()
+	self.curr_college = nbd.StringProperty()
+	self.past_colleges = nbd.StringProperty() #list
+	self.highschool = nbd.StringProperty()
+	self.profile_picture = nbd.BlobProperty() #default not indexed
+	self.curr_job = nbd.StringProperty()
+	self.past_job = nbd.StringProperty()
+	self.about = nbd.TextProperty() #default not indexed
+	self.approach_status = nbd.StringProperty()
+
 	def __init__(self, profile_dict):
-		self.id = profile_dict['profile_type']
+		self.key = nbd.Key(profile_dict['profile_type'], profile_dict['email'])
+		self.username = profile_dict['email']
+		self.profile_type = profile_dict['profile_type']
 		self.email = profile_dict['email']
 		self.full_name = profile_dict['full_name']
-		self.qualities = profile_dict['qualities'] #list
-		self.connections = profile_dict['connections'] #list
+		self.qualities = profile_dict['qualities']
+		self.connections = profile_dict['connections']
 		self.address = profile_dict['address']
-		self.curr_classes = profile_dict['curr_classes'] #list, edu only
-		self.past_classes = profile_dict['past_classes'] #list, linkedin/old edu
+		self.curr_classes = profile_dict['curr_classes']
+		self.past_classes = profile_dict['past_classes']
 		self.major = profile_dict['major']
 		self.minor = profile_dict['minor']
 		self.graduation = profile_dict['graduation']
 		self.curr_college = profile_dict['curr_college']
-		self.past_colleges = profile_dict['past_colleges'] #list
+		self.past_colleges = profile_dict['past_colleges']
 		self.highschool = profile_dict['highschool']
 		self.profile_picture = profile_dict['profile_picture']
 		self.curr_job = profile_dict['curr_job']
